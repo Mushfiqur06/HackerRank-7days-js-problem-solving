34c12dc3217d9d5f2d6972c6f8f87393ddbdf5ee
Mushfiqur Rahman <mushfiqur06@gmail.com>
2018-03-29 02:49:36 +0600

    js-variables.js problem solve

---
+++ js-variables.js
@@ -0,0 +1,8 @@
+/**************************************************
+*Problink Link: 
+* https://www.hackerrank.com/contests/7days-javascript/challenges/js-variables
+***************************************************/
+
+
+var newVariable = "My First Variable";
+console.log(newVariable);
