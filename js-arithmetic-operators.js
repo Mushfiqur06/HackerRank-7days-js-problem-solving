893c0fbe9a098d6cdbb8e42fbbe2fc6e12a81283
Mushfiqur Rahman <mushfiqur06@gmail.com>
2018-03-29 02:57:55 +0600

    js-arithmetic-operators.js problem solve

---
+++ js-arithmetic-operators.js
@@ -0,0 +1,14 @@
+/**************************************************
+*Problink Link: 
+* https://www.hackerrank.com/contests/7days-javascript/challenges/js-arithmetic-operators
+***************************************************/
+
+var add = a + b;
+var sub = a - b;
+var mul = a * b;
+var div = a / b;
+a++;
+var inc = a;
+b--;
+var dec = b;
+console.log('');
