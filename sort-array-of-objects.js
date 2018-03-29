3f63a00f0ffc4ec63055bdf2c12911e406f9806f
Mushfiqur Rahman <mushfiqur06@gmail.com>
2018-03-29 14:11:09 +0600

    sort-array-of-objects.js problem solve

---
+++ sort-array-of-objects.js
@@ -0,0 +1,30 @@
+/**************************************************
+*Problink Link: 
+* https://www.hackerrank.com/contests/7days-javascript/challenges/sort-array-of-objects
+***************************************************/
+function sortLibrary() {
+    console.log(library.sort(function(a, b){
+        return a.title > b.title;
+    }))
+} 
+
+// tail starts here
+var library = [
+    {
+        author: 'Bill Gates',
+        title: 'The Road Ahead',
+        libraryID: 1254
+    },
+    {
+        author: 'Steve Jobs',
+        title: 'Walter Isaacson',
+        libraryID: 4264
+    },
+    {
+        author: 'Suzanne Collins',
+        title: 'Mockingjay: The Final Book of The Hunger Games',
+        libraryID: 3245
+    }
+];
+
+sortLibrary();
